df51553064780d5b04e37bf9a1c4eaaac2dd51ca
Sen Morgan <technars@gmail.com>
2021-09-01 23:13:36 +0200

    Added AccelStepper.h, def.h and headers

---
+++ lib/defs/def.h
@@ -0,0 +1,15 @@
+/**************************************************************
+ *  def.h
+ *
+ *  Created on: 01 September 2021
+ *  Author: SenMorgan https://github.com/SenMorgan
+ *  
+ ***************************************************************/
+
+#ifndef __DEF_H_
+#define __DEF_H_
+
+#define PIN_STEP D1
+#define PIN_DIR D2
+
+#endif /* __DEF_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,38 @@
+/**************************************************************
+ *  main.cpp
+ *
+ *  Created on: 01 September 2021
+ *  Author: SenMorgan https://github.com/SenMorgan
+ *  
+ ***************************************************************/
+
+#include "def.h"
+#include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+#include <WiFiManager.h>
+#include <AccelStepper.h>
+
+AccelStepper stepper(1, PIN_STEP, PIN_DIR);
 
 
-void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
 
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
-    //reset saved settings
-    //wifiManager.resetSettings();
-    
-    //set custom ip for portal
-    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+  //fetches ssid and pass from eeprom and tries to connect
+  //if it does not connect it starts an access point with the "Smart Blinds" name
+  //and goes into a blocking loop awaiting configuration
+  wifiManager.autoConnect("Smart Blinds");
 
-    //fetches ssid and pass from eeprom and tries to connect
-    //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
-    wifiManager.autoConnect("AutoConnectAP");
-    //or use this for auto generated name ESP + ChipID
-    //wifiManager.autoConnect();
-
-    
-    //if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
+  stepper.setMaxSpeed(4000.0);
+  stepper.setAcceleration(2000.0); //Make the acc quick
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
-    
+void loop()
+{
 }
